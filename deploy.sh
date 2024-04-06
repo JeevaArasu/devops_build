888a20b398689815257ecdf42eaaa8c5101d89d7
JeevaArasu <144244668+JeevaArasu@users.noreply.github.com>
2024-04-06 19:41:56 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ echo "$DOCKER_PASS" | docker login -u "$DOCKER_USERNAME" --password-stdin docker
 if [[ $GIT_BRANCH == "origin/master" ]]; then
 docker tag capstone jeevaarasu/react-app
 docker push jeevaarasu/react-app
-elif [[ $GIT_BRANCH == "dev" ]]; then
+elif [[ $GIT_BRANCH == "origin/dev" ]]; then
 docker tag capstone jeevaarasu/dev-app
 docker push jeevaarasu/dev-app
 fi
