73e54bad8134c62e4caae1ad71ad3d044e529b9d
JeevaArasu <144244668+JeevaArasu@users.noreply.github.com>
2024-04-07 23:45:14 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 echo "Logging in to Docker Hub..."
 docker login -u jeevaarasu  -p dckr_pat_NXoExep3iTjubmqBCdmHgQ59faE
 if [[ $GIT_BRANCH == "origin/master" ]]; then
-docker tag capstone jeevaarasu/react-app
-docker push jeevaarasu/react-app
+docker tag capstone jeevaarasu/prod:cap
+docker push jeevaarasu/prod:cap
 elif [[ $GIT_BRANCH == "origin/dev" ]]; then
-docker tag capstone jeevaarasu/dev-app
-docker push jeevaarasu/dev-app
+docker tag capstone jeevaarasu/dev:app
+docker push jeevaarasu/dev:app
 fi
