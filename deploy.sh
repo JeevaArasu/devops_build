3aa0160f685ddce076ce5f0c7f7cc614d15f8d59
JeevaArasu <144244668+JeevaArasu@users.noreply.github.com>
2024-04-08 12:58:04 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Logging in to Docker Hub..."
-docker login -u jeevaarasu  -p dckr_pat_NXoExep3iTjubmqBCdmHgQ59faE
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASS
+echo "$DOCKER_PASS" | docker login -u "$DOCKER_USERNAME" --password-stdin docker.io
 if [[ $GIT_BRANCH == "origin/master" ]]; then
 docker tag capstone jeevaarasu/prod:cap
 docker push jeevaarasu/prod:cap
